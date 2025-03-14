3b47738331c14dc7dfc997ebe48463ffeb8c1841
Brahia-A <10114@holbertonstudents.com>
2025-03-14 14:27:24 -0300

    se modifico el metodo create_app, para recibir una configuración, que se utilizará para instanciar la aplicación.

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,18 +1,29 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
-
-def create_app():
+def create_app(config_class=None):
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Register the users namespace
+    # Cargar la configuración por defecto desde la clase
+    if config_class:
+        app.config.from_object(config_class)
+    else:
+        from config import DevelopmentConfig
+        app.config.from_object(DevelopmentConfig)
+
+    # Inicializa Flask-REST y CORS
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    CORS(app)
+
+    # Registra los namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
     return app
