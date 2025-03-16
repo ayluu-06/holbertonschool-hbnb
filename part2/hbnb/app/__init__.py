4ebe25312e0ec89fc68463bd2b5f9a67f40d765c
Brahia-A <10114@holbertonstudents.com>
2025-03-16 15:22:32 -0300

    configuracion del entorno

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -6,9 +6,15 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from flask_jwt_extended import JWTManager
+
 
 def create_app(config_class=None):
     app = Flask(__name__)
+    app.config['JWT_SECRET_KEY'] = 'my_super_secret_key'  # Clave secreta para generar tokens
+    
+    bcrypt = Bcrypt(app)
+    jwt = JWTManager(app)
 
     # Cargar la configuración por defecto desde la clase
     if config_class:
@@ -17,7 +23,7 @@ def create_app(config_class=None):
         from config import DevelopmentConfig
         app.config.from_object(DevelopmentConfig)
     
-    bcrypt = Bcrypt()
+    
 
     # Inicializa Flask-REST y CORS
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/brahian/holbertonschool-hbnb-8/venv
+set -gx VIRTUAL_ENV /home/brahian/holbertonschool-hbnb-9/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
